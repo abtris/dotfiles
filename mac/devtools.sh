3dc9ac434d047732a181dbfecfd61fd3f35d5db5
Ladislav Prskavec <ladislav@prskavec.net>
2022-02-02 20:17:04 +0100

    fix: cleanup old files

--- mac/devtools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo
-echo "  → Removing DeveloperTools and installing GCC..."
-sudo /Developer/Library/uninstall-devtools –mode=all
-curl --progress-bar --insecure --location --output /tmp/GCC-10.7-v2.pkg https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg
-open /tmp/GCC-10.7-v2.pkg
-
-echo "✓ Install GCC with via the graphical installer"
