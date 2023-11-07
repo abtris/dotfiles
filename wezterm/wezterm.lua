a2ce686b4f0f934445154ddd03e8b9d7ecb12e4a
Ladislav Prskavec <ladislav@prskavec.net>
2023-11-07 09:38:34 +0100

    Add wezterm keybindigs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,44 @@ if wezterm.config_builder then
 	config:set_strict_mode(false)
 end
 
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+	  {
+    key = 'd',
+    mods = 'SHIFT|CMD',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+	  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'CMD',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'CMD',
+    action = act.ActivatePaneDirection 'Down',
+  },
+}
+
 local process_icons = {
 	["docker"] = {
 		Text = nf.linux_docker,
@@ -73,6 +111,44 @@ local process_icons = {
 		Text = nf.dev_github_badge,
 	},
 }
+
+wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, hover, _max_width)
+	local has_unseen_output = false
+	for _, pane in ipairs(tab.panes) do
+		if pane.has_unseen_output then
+			has_unseen_output = true
+			break
+		end
+	end
+	local title = get_proc_title(tab.active_pane)
+
+	local icon
+	if process_icons[title] ~= nil then
+		icon = wezterm.format({
+			{ Text = " " },
+			process_icons[title],
+			{ Text = " " },
+		})
+	else
+		icon = wezterm.format({
+			{ Text = string.format(" %s", title) },
+		})
+	end
+	return {
+		hover and {
+			Background = { Color = "#2a2e36" },
+		} or "ResetAttributes",
+		{
+			Foreground = {
+				Color = has_unseen_output and palette.lemon_chiffon
+					or (tab.is_active and palette.turquoise or "#9196c2"),
+			},
+		},
+		{ Text = icon },
+		{ Foreground = { Color = "#9196c2" } },
+		{ Text = tab.tab_title == "" and title or tab.tab_title },
+	}
+end)
 
 config.font_size = 18.0
 config.color_scheme = "Catppuccin Macchiato" -- or Macchiato, Frappe, Latte
