a836f2b8b011d430f5ba1a2dc487d94d977f9f01
Ladislav Prskavec <ladislav@prskavec.net>
2023-12-01 16:25:30 +0100

    Better tab title, remove just home dir

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,84 @@ if wezterm.config_builder then
 	config:set_strict_mode(false)
 end
 
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane.current_working_dir
+	myHome = string.format("/Users/%s", os.getenv("USER"))
+	index = string.find(current_dir, myHome, 1, false)
+	return string.sub(current_dir, index + string.len(myHome))
+end
+
+local process_icons = {
+	['docker'] = wezterm.nerdfonts.linux_docker,
+	['docker-compose'] = wezterm.nerdfonts.linux_docker,
+	['psql'] = '󱤢',
+	['usql'] = '󱤢',
+	['kuberlr'] = wezterm.nerdfonts.linux_docker,
+	['kubectl'] = wezterm.nerdfonts.linux_docker,
+	['stern'] = wezterm.nerdfonts.linux_docker,
+	['nvim'] = wezterm.nerdfonts.custom_vim,
+	['make'] = wezterm.nerdfonts.seti_makefile,
+	['vim'] = wezterm.nerdfonts.dev_vim,
+	['node'] = wezterm.nerdfonts.mdi_hexagon,
+	['go'] = wezterm.nerdfonts.seti_go,
+	['zsh'] = wezterm.nerdfonts.dev_terminal,
+	['bash'] = wezterm.nerdfonts.cod_terminal_bash,
+	['btm'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+	['cargo'] = wezterm.nerdfonts.dev_rust,
+	['sudo'] = wezterm.nerdfonts.fa_hashtag,
+	['lazydocker'] = wezterm.nerdfonts.linux_docker,
+	['git'] = wezterm.nerdfonts.dev_git,
+	['lua'] = wezterm.nerdfonts.seti_lua,
+	['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
+	['curl'] = wezterm.nerdfonts.mdi_flattr,
+	['gh'] = wezterm.nerdfonts.dev_github_badge,
+	['ruby'] = wezterm.nerdfonts.cod_ruby,
+}
+
+local function get_process(tab)
+	if not tab.active_pane or tab.active_pane.foreground_process_name == '' then
+		return '[?]'
+	end
+
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
+	if string.find(process_name, 'kubectl') then
+		process_name = 'kubectl'
+	end
+
+	return process_icons[process_name] or string.format('[%s]', process_name)
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+	local has_unseen_output = false
+	if not tab.is_active then
+		for _, pane in ipairs(tab.panes) do
+			if pane.has_unseen_output then
+				has_unseen_output = true
+				break
+			end
+		end
+	end
+
+	local cwd = wezterm.format({
+		{ Attribute = { Intensity = 'Bold' } },
+		{ Text = get_current_working_dir(tab) },
+	})
+
+	local title = string.format(' %s ~ %s  ', get_process(tab), cwd)
+
+	if has_unseen_output then
+		return {
+			{ Foreground = { Color = '#28719c' } },
+			{ Text = title },
+		}
+	end
+
+	return {
+		{ Text = title },
+	}
+end)
+
 -- https://wezfurlong.org/wezterm/hyperlinks.html#implicit-hyperlinks
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
@@ -25,10 +103,10 @@ config.keys = {
 		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
 	},
 	{
-    key = 'Enter',
-    mods = 'CMD',
-    action = wezterm.action.TogglePaneZoomState,
-  },
+		key = 'Enter',
+		mods = 'CMD',
+		action = wezterm.action.TogglePaneZoomState,
+	},
 	{
 		key = 'w',
 		mods = 'CMD',
@@ -72,7 +150,8 @@ config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
-config.tab_max_width = 50
+
+config.tab_max_width = 100
 
 config.window_close_confirmation = 'NeverPrompt'
 
